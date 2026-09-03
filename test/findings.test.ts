erfianugrah/sbperf

fix: scope index/RLS/vacuum signals to app schemas, not public-only
The findings engine and the narrate digest decided "is this the user's
table?" with `schema === "public"`, which silently reported ZERO unused/
duplicate/unindexed-RLS objects for any project keeping its tables in a
custom schema, while the schema-aware advisor path flagged them - the two
diverged. Replace the heuristic with a shared app-schema denylist.

- src/appschema.ts: isAppSchema()/appRows() + NON_APP_SCHEMAS, mirrored
  from splinter.sql's unused_index exclusion set (never flag a Supabase-
  managed index, never miss a custom app schema). Exports NON_APP_SCHEMAS_SQL
  so the SQL layer applies the same scope server-side.
- findings.ts: appRows for the SQL index/RLS findings + positives; the
  unused/duplicate index SQL findings are now fallbacks, suppressed when the
  advisor's own lint already fired (no more double-reporting the same objects).
- narrate.ts: appRows + NAMED unused indexes, dead-tuple/autovacuum lag,
  per-table write profile, outdated extensions in the digest; dead-tuple and
  write-profile rows scoped app-only. buildNarrativeInput is the sole digest
  projection for both the LLM and the print-prompt round-trip.
- sql.ts: index-stats/duplicate-indexes/seq-scan-heavy exclude managed
  schemas IN SQL (NON_APP_SCHEMAS_SQL) so a project with many managed indexes
  can't crowd the user's own out of the row cap; index-stats cap 30->50.
  rlsUnindexed deliberately keeps storage (bucket RLS is user-authored).
- scripts/check-schemas-drift.ts + check:schemas: enforce the superset
  invariant (NON_APP_SCHEMAS must contain every schema splinter excludes);
  wired into CI alongside the now-also-wired check:lints.
- tests: custom-schema inclusion, managed-schema exclusion, advisor-fallback
  dedup, new digest fields.
- docs: AGENTS.md conventions + bounded contexts, docs/heuristics.md.

test/findings.test.ts
@@ -184,18 +184,32 @@ describe("deriveFindings", () => {
     expect(f?.dashUrl).toContain("/advisors/performance");
   });
 
-  test("only public-schema unused indexes counted", () => {
+  test("unused indexes: application schemas counted, Supabase-managed excluded", () => {
     const a = base();
     a.sql.indexStats = [
-      { schema: "auth", table: "auth.users", index: "auth.i1", unused: true },
-      { schema: "public", table: "public.x", index: "public.i2", unused: true },
+      { schema: "auth", table: "auth.users", index: "auth.i1", unused: true }, // managed -> excluded
+      { schema: "storage", table: "storage.objects", index: "storage.i5", unused: true }, // managed
+      { schema: "public", table: "public.x", index: "public.i2", unused: true }, // app
+      { schema: "cs2a", table: "cs2a.matches", index: "cs2a.i4", unused: true }, // custom app schema
       { schema: "public", table: "public.x", index: "public.i3", unused: false },
     ];
     const f = deriveFindings(a).find((x) => x.anchor === "#unused");
-    expect(f?.title).toContain("1 unused index in public");
+    // public.i2 + cs2a.i4 (auth/storage dropped); wording is schema-neutral
+    expect(f?.title).toContain("2 unused indexes");
+    expect(f?.title).not.toContain("in public");
   });
 
-  test("duplicate indexes in public -> med Performance finding", () => {
+  test("advisor unused_index lint suppresses the SQL fallback (no double-report)", () => {
+    const a = base();
+    a.advisors.performance = [{ name: "unused_index", title: "Unused Index", level: "INFO" }];
+    a.sql.indexStats = [{ schema: "cs2a", table: "cs2a.t", index: "cs2a.i", unused: true }];
+    const perf = deriveFindings(a).filter((x) => x.category === "Performance");
+    // exactly one unused-index card - the advisor's, not the SQL fallback
+    expect(perf.filter((x) => x.anchor === "#unused")).toHaveLength(0);
+    expect(perf.some((x) => x.anchor === "#adv-perf")).toBe(true);
+  });
+
+  test("duplicate indexes -> med Performance finding (managed schema excluded)", () => {
     const a = base();
     a.sql.duplicateIndexes = [
       { schema: "public", table: "public.x", indexes: "i1, i2", copies: 2 },
@@ -204,7 +218,8 @@ describe("deriveFindings", () => {
     const f = deriveFindings(a).find((x) => x.anchor === "#dupidx");
     expect(f?.severity).toBe("med");
     expect(f?.category).toBe("Performance");
-    expect(f?.title).toContain("1 public table has duplicate indexes");
+    expect(f?.title).toContain("1 table has duplicate indexes");
+    expect(f?.title).not.toContain("public");
   });
 
   test("RLS policy columns without an index -> med Performance finding", () => {

test/narrate.test.ts
@@ -132,6 +132,62 @@ describe("buildNarrativeInput", () => {
     expect(input.evidence.scheduledJobs).toHaveLength(1);
   });
 
+  test("digest names unused indexes (app-scoped) and surfaces dead-tuple + write-profile signals", () => {
+    const a = base();
+    a.sql.indexStats = [
+      { schema: "auth", table: "auth.users", index: "auth.i1", unused: true }, // managed -> dropped
+      {
+        schema: "cs2a",
+        table: "cs2a.matches",
+        index: "cs2a.pg_x_idx",
+        index_size: "192 kB",
+        scans: 0,
+        unused: true,
+      },
+      { schema: "public", table: "public.y", index: "public.y_pkey", scans: 500, unused: false },
+    ];
+    a.sql.deadTuples = [
+      // managed schema with the MOST dead rows - must not sort to the top (excluded)
+      {
+        schema: "auth",
+        table: "auth.refresh_tokens",
+        dead_rows: 99999,
+        live_rows: 1,
+        overdue: "yes",
+      },
+      { schema: "cs2a", table: "cs2a.small", dead_rows: 10, live_rows: 100, overdue: "no" },
+      { schema: "cs2a", table: "cs2a.hot", dead_rows: 9000, live_rows: 1000, overdue: "yes" },
+    ];
+    a.sql.trafficProfile = [
+      { table: "cs2a.hot", profile: "567.4:1 write-heavy", write_tuples: 8298, blocks_read: 2 },
+      {
+        table: "storage.objects",
+        profile: "900:1 write-heavy",
+        write_tuples: 99999,
+        blocks_read: 1,
+      },
+    ];
+    a.sql.extensions = [
+      { name: "pg_stat_statements", installed: "1.11", latest: "1.11", outdated: false },
+      { name: "pgcrypto", installed: "1.2", latest: "1.3", outdated: true },
+    ];
+    const input = buildNarrativeInput(a);
+    // named unused index, auth-managed one excluded
+    expect(input.evidence.unusedIndexes).toBe(1);
+    expect(input.evidence.unusedIndexesTop).toHaveLength(1);
+    expect(input.evidence.unusedIndexesTop[0]?.index).toBe("cs2a.pg_x_idx");
+    // dead tuples: managed auth row excluded; overdue cs2a row sorted first
+    expect(input.evidence.deadTuples.map((r) => r.table)).not.toContain("auth.refresh_tokens");
+    expect(input.evidence.deadTuples[0]?.table).toBe("cs2a.hot");
+    expect(input.evidence.deadTuples[0]?.overdue).toBe("yes");
+    // write-profile: managed storage row excluded, app row surfaced
+    expect(input.evidence.writeHeavyTables.map((r) => r.table)).not.toContain("storage.objects");
+    expect(input.evidence.writeHeavyTables[0]?.table).toBe("cs2a.hot");
+    expect(input.evidence.extensionsOutdated).toEqual([
+      { name: "pgcrypto", installed: "1.2", latest: "1.3" },
+    ]);
+  });
+
   test("flags degraded collection so the model can caveat", () => {
     const a = base();
     a.meta.status = "INACTIVE";

erfianugrah/pg-analyser

test/findings.test.ts
@@ -184,18 +184,32 @@ describe("deriveFindings", () => {
     expect(f?.dashUrl).toContain("/advisors/performance");
   });
 
-  test("only public-schema unused indexes counted", () => {
+  test("unused indexes: application schemas counted, Supabase-managed excluded", () => {
     const a = base();
     a.sql.indexStats = [
-      { schema: "auth", table: "auth.users", index: "auth.i1", unused: true },
-      { schema: "public", table: "public.x", index: "public.i2", unused: true },
+      { schema: "auth", table: "auth.users", index: "auth.i1", unused: true }, // managed -> excluded
+      { schema: "storage", table: "storage.objects", index: "storage.i5", unused: true }, // managed
+      { schema: "public", table: "public.x", index: "public.i2", unused: true }, // app
+      { schema: "app1", table: "app1.matches", index: "app1.i4", unused: true }, // custom app schema
       { schema: "public", table: "public.x", index: "public.i3", unused: false },
     ];
     const f = deriveFindings(a).find((x) => x.anchor === "#unused");
-    expect(f?.title).toContain("1 unused index in public");
+    // public.i2 + app1.i4 (auth/storage dropped); wording is schema-neutral
+    expect(f?.title).toContain("2 unused indexes");
+    expect(f?.title).not.toContain("in public");
   });
 
-  test("duplicate indexes in public -> med Performance finding", () => {
+  test("advisor unused_index lint suppresses the SQL fallback (no double-report)", () => {
+    const a = base();
+    a.advisors.performance = [{ name: "unused_index", title: "Unused Index", level: "INFO" }];
+    a.sql.indexStats = [{ schema: "app1", table: "app1.t", index: "app1.i", unused: true }];
+    const perf = deriveFindings(a).filter((x) => x.category === "Performance");
+    // exactly one unused-index card - the advisor's, not the SQL fallback
+    expect(perf.filter((x) => x.anchor === "#unused")).toHaveLength(0);
+    expect(perf.some((x) => x.anchor === "#adv-perf")).toBe(true);
+  });
+
+  test("duplicate indexes -> med Performance finding (managed schema excluded)", () => {
     const a = base();
     a.sql.duplicateIndexes = [
       { schema: "public", table: "public.x", indexes: "i1, i2", copies: 2 },
@@ -204,7 +218,8 @@ describe("deriveFindings", () => {
     const f = deriveFindings(a).find((x) => x.anchor === "#dupidx");
     expect(f?.severity).toBe("med");
     expect(f?.category).toBe("Performance");
-    expect(f?.title).toContain("1 public table has duplicate indexes");
+    expect(f?.title).toContain("1 table has duplicate indexes");
+    expect(f?.title).not.toContain("public");
   });
 
   test("RLS policy columns without an index -> med Performance finding", () => {

test/narrate.test.ts
@@ -132,6 +132,62 @@ describe("buildNarrativeInput", () => {
     expect(input.evidence.scheduledJobs).toHaveLength(1);
   });
 
+  test("digest names unused indexes (app-scoped) and surfaces dead-tuple + write-profile signals", () => {
+    const a = base();
+    a.sql.indexStats = [
+      { schema: "auth", table: "auth.users", index: "auth.i1", unused: true }, // managed -> dropped
+      {
+        schema: "app1",
+        table: "app1.matches",
+        index: "app1.pg_x_idx",
+        index_size: "192 kB",
+        scans: 0,
+        unused: true,
+      },
+      { schema: "public", table: "public.y", index: "public.y_pkey", scans: 500, unused: false },
+    ];
+    a.sql.deadTuples = [
+      // managed schema with the MOST dead rows - must not sort to the top (excluded)
+      {
+        schema: "auth",
+        table: "auth.refresh_tokens",
+        dead_rows: 99999,
+        live_rows: 1,
+        overdue: "yes",
+      },
+      { schema: "app1", table: "app1.small", dead_rows: 10, live_rows: 100, overdue: "no" },
+      { schema: "app1", table: "app1.hot", dead_rows: 9000, live_rows: 1000, overdue: "yes" },
+    ];
+    a.sql.trafficProfile = [
+      { table: "app1.hot", profile: "567.4:1 write-heavy", write_tuples: 8298, blocks_read: 2 },
+      {
+        table: "storage.objects",
+        profile: "900:1 write-heavy",
+        write_tuples: 99999,
+        blocks_read: 1,
+      },
+    ];
+    a.sql.extensions = [
+      { name: "pg_stat_statements", installed: "1.11", latest: "1.11", outdated: false },
+      { name: "pgcrypto", installed: "1.2", latest: "1.3", outdated: true },
+    ];
+    const input = buildNarrativeInput(a);
+    // named unused index, auth-managed one excluded
+    expect(input.evidence.unusedIndexes).toBe(1);
+    expect(input.evidence.unusedIndexesTop).toHaveLength(1);
+    expect(input.evidence.unusedIndexesTop[0]?.index).toBe("app1.pg_x_idx");
+    // dead tuples: managed auth row excluded; overdue app1 row sorted first
+    expect(input.evidence.deadTuples.map((r) => r.table)).not.toContain("auth.refresh_tokens");
+    expect(input.evidence.deadTuples[0]?.table).toBe("app1.hot");
+    expect(input.evidence.deadTuples[0]?.overdue).toBe("yes");
+    // write-profile: managed storage row excluded, app row surfaced
+    expect(input.evidence.writeHeavyTables.map((r) => r.table)).not.toContain("storage.objects");
+    expect(input.evidence.writeHeavyTables[0]?.table).toBe("app1.hot");
+    expect(input.evidence.extensionsOutdated).toEqual([
+      { name: "pgcrypto", installed: "1.2", latest: "1.3" },
+    ]);
+  });
+
   test("flags degraded collection so the model can caveat", () => {
     const a = base();
     a.meta.status = "INACTIVE";